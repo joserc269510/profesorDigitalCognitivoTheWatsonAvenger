c61f45146cbd677070fe144f480471de6ce47dcc
estebanmorales1594 <estebanmorales1594@hotmail.com>
2017-04-26 17:06:44 -0600

    Esteban Morales
    5:05 26\04

---
+++ src/main/java/Servlet/ServletSpeech.java
@@ -0,0 +1,53 @@
+package Servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletSpeech
+ */
+@WebServlet("/ServletSpeech")
+public class ServletSpeech extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletSpeech() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String texto = null;
+		
+		Speech sp=new Speech();
+		try {
+			texto=sp.voz_a_texto();
+		} catch (LineUnavailableException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+
+		 request.setAttribute("texto", texto);
+		 RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+		 dispatcher.forward(request, response);
+	}
+
+}
