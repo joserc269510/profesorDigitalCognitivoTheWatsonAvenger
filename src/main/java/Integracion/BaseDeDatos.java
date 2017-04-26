42168701d2358e0c346cc5f3ddef71560d166d32
joserc269510 <jose26jose26>
2017-04-26 16:08:27 -0600

    4:08 26 abril
    hola

--- src/main/java/Integracion/BaseDeDatos.java
+++ src/main/java/Integracion/BaseDeDatos.java
@@ -9,28 +9,29 @@ import java.sql.SQLException;
 
 public class BaseDeDatos {
 
-  public static void main(String[] args) {
-	        try {
+	public static final String url ="jdbc:postgresql://echo.db.elephantsql.com:5432/syvmjttg";
+	public static final String username = "syvmjttg";
+	public static final String password = "MFVYQ1dzq8OSpqf7P3t6IFk-9rYyBi8q";
+
+	
+	public void select(String query){//selects
+	       	try {
 	            Class.forName("org.postgresql.Driver");
 	        }
 	        catch (java.lang.ClassNotFoundException e) {
 	            System.out.println(e.getMessage() + "hola");
 	        }
-	        String url ="jdbc:postgresql://echo.db.elephantsql.com:5432/syvmjttg";
-	        String username = "syvmjttg";
-	        String password = "MFVYQ1dzq8OSpqf7P3t6IFk-9rYyBi8q";
-
 	        try {
 	        	System.out.println("daskdjkasjdsakl");
 	            Connection db = DriverManager.getConnection(url, username, password);
 	            Statement st = db.createStatement();
-	            ResultSet rs = st.executeQuery("SELECT * FROM public.profesor LIMIT 100");
+	            ResultSet rs = st.executeQuery(query);
 	            while (rs.next()) {
 	                System.out.print("Column 1 returned ");
 	                System.out.println(rs.getString(2));
 	                System.out.print("Column 2 returned ");
 	                System.out.println(rs.getString(3));	              
-	            }	           
+	            }
 	            rs.close();
 	            st.close();
 	            }
@@ -38,5 +39,33 @@ public class BaseDeDatos {
 	            System.out.println(e.getMessage() + "adios");
 	        }
 	    }
+	
+	public void insertDelete(String query){
+       	try {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (java.lang.ClassNotFoundException e) {
+            System.out.println(e.getMessage() + "hola");
+        }
+       	
+       	try{
+       		System.out.println("daskdjkasjdsakl");
+            Connection db = DriverManager.getConnection(url, username, password);
+            Statement st = db.createStatement();
+            st.executeUpdate(query);  
+            st.close();
+       	}
+       	catch(java.sql.SQLException e){
+       		System.out.println(e.getMessage() + "adios");
+       	}
+	}
+	
+	public void update(String query){
+		
+	}
+  
+  	public static void main(String[] args) {
+	
+  	}
 
 	}
