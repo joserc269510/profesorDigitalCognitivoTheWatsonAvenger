9f179c602eefa3fcd70d5a3e6245973e81bc2c2e
estebanmorales1594 <estebanmorales1594@hotmail.com>
2017-04-26 19:32:18 -0600

    Servlets

---
+++ src/main/java/Integracion/Speech.java
@@ -0,0 +1,72 @@
+package Integracion;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
+
+import com.ibm.watson.developer_cloud.http.HttpMediaType;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.BaseRecognizeCallback;
+
+public class Speech {
+	SpeechResults transcript=null;
+
+	public String voz_a_texto() throws LineUnavailableException, InterruptedException
+	{
+		
+		String texto=null;
+		int sampleRate = 16000;
+		
+		SpeechToText s2t = new SpeechToText();
+		/*s2t.setUsernameAndPassword("4a76e4e6-2f33-424f-a847-c06e5d3551f0", "tJwskp6qa47O");*/
+		s2t.setUsernameAndPassword("c0e1dc84-14d0-41e2-b673-ca20b419cb5a", "qqrtXDD5YO0f");
+
+		 AudioFormat format = new AudioFormat(sampleRate, 16, 1, true, false);
+		 DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+		 if (!AudioSystem.isLineSupported(info))
+		 {
+		     System.out.println("Line not supported");
+		     System.exit(0);
+		  }
+		 TargetDataLine line = (TargetDataLine) AudioSystem.getLine(info);
+		 line.open(format);
+		 line.start();
+		 AudioInputStream audio2 = new AudioInputStream(line);
+		
+		RecognizeOptions options = new RecognizeOptions.Builder().model("es-ES_NarrowbandModel")
+		     .continuous(true)
+		     .interimResults(true)
+		     .contentType(HttpMediaType.AUDIO_RAW + "; rate=" + sampleRate)
+		     .build();
+	        
+		s2t.recognizeUsingWebSocket(audio2, options, new BaseRecognizeCallback() 
+		{
+			public void onTranscription(SpeechResults speechResults) 
+			{
+				transcript=speechResults;
+		       //System.out.println(speechResults);
+		     }
+		});
+
+		System.out.println("Listening to your voice for the next 30s...");
+		Thread.sleep(5 * 1000);
+		
+		try{
+   	   	 	texto=transcript.getResults().get(0).getAlternatives().get(0).getTranscript();
+		}catch(Exception e){
+			 texto="Line not supported";
+		}
+		 // closing the WebSockets underlying InputStream will close the WebSocket itself.
+		 line.stop();
+		 line.close();
+		 System.out.println("Fin.");
+		   
+		 return texto;
+		 
+	}
+}
--- src/main/java/Servlet/servletRegistrarEstudiante.java
+++ src/main/java/Servlet/servletRegistrarEstudiante.java
@@ -1,28 +1,25 @@
 package Servlet;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sound.sampled.LineUnavailableException;
 
-import Integracion.Speech;
+import logicaDeNegocios.Estudiante;
 
 /**
- * Servlet implementation class ServletSpeech
+ * Servlet implementation class servletRegistrarEstudiante
  */
-@WebServlet("/ServletSpeech")
-public class ServletSpeech extends HttpServlet {
+@WebServlet("/servletRegistrarEstudiante")
+public class servletRegistrarEstudiante extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletSpeech() {
+    public servletRegistrarEstudiante() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -39,20 +36,18 @@ public class ServletSpeech extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String texto = null;
-		
-		Speech sp=new Speech();
-		try {
-			texto=sp.voz_a_texto();
-		} catch (LineUnavailableException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 
-
-		 request.setAttribute("texto", texto);
-		 RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
-		 dispatcher.forward(request, response);
+		String numeroIdentificacion= request.getParameter("txtIdentificacion");
+	   	 String numeroCarnet= request.getParameter("txtCarnet"); 
+	   	 String fechaNacimiento =request.getParameter("txtNacimiento");
+	   	 String apellido1=request.getParameter("txtApellido1") ;
+	   	 String apellido2= request.getParameter("txtApellido2");
+	   	 String nombre= request.getParameter("txtNombre");
+	   	 String email= request.getParameter("txtEmail");
+	   	 
+	   Estudiante estudiante= new Estudiante();
+	   estudiante.registrarEstudiante(numeroIdentificacion, numeroCarnet, fechaNacimiento, apellido1, apellido2, nombre, email);
+	 
+	   response.setContentType("text/html");
 	}
 
 }
