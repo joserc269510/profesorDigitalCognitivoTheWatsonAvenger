0ae3bb0546e39220840ce24ffeb63a632e799a27
joserc269510 <jose26jose26>
2017-04-28 10:01:34 -0600

    3:00 viernes
    hola

--- src/main/java/Integracion/BaseDeDatos.java
+++ src/main/java/Integracion/BaseDeDatos.java
@@ -12,10 +12,34 @@ public class BaseDeDatos {
 	public static final String url ="jdbc:postgresql://echo.db.elephantsql.com:5432/syvmjttg";
 	public static final String username = "syvmjttg";
 	public static final String password = "MFVYQ1dzq8OSpqf7P3t6IFk-9rYyBi8q";
-
 	
-	public void select(String query){//selects
-	       	try {
+	public int getNumeroRegistros(String tabla, String columna){
+		int index=0;
+		try {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch (java.lang.ClassNotFoundException e) {
+            System.out.println(e.getMessage() + "hola");
+        }
+        try {
+            Connection db = DriverManager.getConnection(url, username, password);
+            Statement st = db.createStatement();
+            ResultSet rs = st.executeQuery( "select count(" + columna + ") from " + tabla);
+            while (rs.next()) {
+            	index = rs.getInt(1);            
+            }
+            rs.close();
+            st.close();
+            }
+        catch (java.sql.SQLException e) {
+        }
+        return index;
+	}
+	
+	public Object[] select (String query){
+	       	
+			
+			try {
 	            Class.forName("org.postgresql.Driver");
 	        }
 	        catch (java.lang.ClassNotFoundException e) {
@@ -26,11 +50,11 @@ public class BaseDeDatos {
 	            Connection db = DriverManager.getConnection(url, username, password);
 	            Statement st = db.createStatement();
 	            ResultSet rs = st.executeQuery(query);
+	            
+	            int index=0;
+	            //int indexMatriz=0;
 	            while (rs.next()) {
-	                System.out.print("Column 1 returned ");
-	                System.out.println(rs.getString(2));
-	                System.out.print("Column 2 returned ");
-	                System.out.println(rs.getString(3));	              
+	            	System.out.println(rs.getString(index)+  "kdasjdk");              
 	            }
 	            rs.close();
 	            st.close();
@@ -39,6 +63,7 @@ public class BaseDeDatos {
 	            System.out.println(e.getMessage() + "adios");
 	        }
 	    }
+	
 	
 	public void insertDelete(String query){
        	try {
@@ -67,8 +92,12 @@ public class BaseDeDatos {
   	public static void main(String[] args) 
   	{
   		BaseDeDatos b= new BaseDeDatos();
+  		//select count (nombreestudiante) from estudiante
+  		//insert into estudiante values ('3485081','Jose', 'Ramirez', 'Calderon', '2014077444','2/5/1994','jose12.13@hotmail.com');
   		//b.insertDelete("insert into estudiante values( "1-1235-345","Coraima",'Fonseca', 'Alvarado','201210915','2/5/1994','cora2994@gmail.com')");
   		//b.select("Select * from estudiante");
+  		//b.select("Select * from estudiante",1);
+  		b.getNumeroRegistros("estudiante", "nombreestudiante");
   	}
 
 	}
---
+++ src/main/java/logicaDeNegocios/pregunta.java
@@ -0,0 +1,36 @@
+package logicaDeNegocios;
+
+public class pregunta {
+	int codigoPregunta;
+	int codSubtema;
+	String descripcionPregunta;
+	String descripcionAyuda;
+	
+	
+	public int getCodigoPregunta() {
+		return codigoPregunta;
+	}
+	public void setCodigoPregunta(int codigoPregunta) {
+		this.codigoPregunta = codigoPregunta;
+	}
+	public int getCodSubtema() {
+		return codSubtema;
+	}
+	public void setCodSubtema(int codSubtema) {
+		this.codSubtema = codSubtema;
+	}
+	public String getDescripcionPregunta() {
+		return descripcionPregunta;
+	}
+	public void setDescripcionPregunta(String descripcionPregunta) {
+		this.descripcionPregunta = descripcionPregunta;
+	}
+	public String getDescripcionAyuda() {
+		return descripcionAyuda;
+	}
+	public void setDescripcionAyuda(String descripcionAyuda) {
+		this.descripcionAyuda = descripcionAyuda;
+	}
+	
+	
+}
