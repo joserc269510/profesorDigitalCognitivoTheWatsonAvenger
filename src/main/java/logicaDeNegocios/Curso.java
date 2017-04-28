ac6e4ca22e5937b323cff6fb5e8c8552de92a9ea
Cora2994 <cora2994@gmail.com>
2017-04-28 15:00:05 -0600

    1459
    Insert Curso

--- src/main/java/logicaDeNegocios/Curso.java
+++ src/main/java/logicaDeNegocios/Curso.java
@@ -1,7 +1,40 @@
 package logicaDeNegocios;
 
+import Integracion.BaseDeDatos;
+
 public class Curso 
 {
-
+	String codigo; 
+	String descripcionCurso;
+	
+	
+	public void RegistrarCurso(String pCodigo, String pDescripcion)
+	{
+		setCodigo(pCodigo);
+		setDescripcionCurso(pDescripcion);
+		BaseDeDatos conexion= new BaseDeDatos();
+		conexion.insertDelete("insert into curso values (" + "'"+ pCodigo + "'" + "," + "'" + pDescripcion +"'" + ")");
+	}
+	
+	public String getCodigo() 
+	{
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) 
+	{
+		this.codigo = codigo;
+	}
+	
+	public String getDescripcionCurso() 
+	{
+		return descripcionCurso;
+	}
+	
+	public void setDescripcionCurso(String descripcionCurso) 
+	{
+		this.descripcionCurso = descripcionCurso;
+	} 
+	
 	
 }
