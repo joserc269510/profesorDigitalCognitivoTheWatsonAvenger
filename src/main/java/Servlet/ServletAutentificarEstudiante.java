2b03f4d1f251c8b5878a8831370e5359fc1507c5
Cora2994 <cora2994@gmail.com>
2017-06-08 15:17:39 -0600

    cambios para Archivo

--- src/main/java/Servlet/ServletAutentificarEstudiante.java
+++ src/main/java/Servlet/ServletAutentificarEstudiante.java
@@ -1,8 +1,10 @@
 package Servlet;
 
 import java.awt.Toolkit;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,6 +13,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 
 import Integracion.BaseDeDatos;
@@ -38,11 +45,11 @@ public class ServletAutentificarEstudiante extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{	
-		Archivo archivo= new Archivo();
+	
 		String ruta = null;
 		try {
 	
-			ruta=  archivo.CargarImagen(request, response);
+			ruta=  CargarImagen(request, response);
 			System.out.println(ruta +"Hola");
 	
 		} 
@@ -50,7 +57,7 @@ public class ServletAutentificarEstudiante extends HttpServlet {
 		{
 			e.printStackTrace();
 		}
-		String nombreImagen= archivo.obtenerNombreImagen(ruta);  
+		String nombreImagen= obtenerNombreImagen(ruta);  
 		
 		Estudiante estudiante = new Estudiante();
 		Boolean resp;
@@ -84,4 +91,87 @@ public class ServletAutentificarEstudiante extends HttpServlet {
 	      }
 	}
 
+
+
+
+
+public String CargarImagen(HttpServletRequest request, HttpServletResponse response) throws Exception
+{
+	//Ruta donde se guardara el fichero
+	//File destino=new File("/Users/joseramirezcalderon/Desktop/profesorDigitalCognitivoTheWatsonAvenger");
+	File destino=new File("D:\\Users\\Esteban Morales\\git\\profesorDigitalCognitivoTheWatsonAvenger");
+	FileItemFactory factory = new DiskFileItemFactory();
+	ServletFileUpload upload = new ServletFileUpload(factory);
+
+	// req es la HttpServletRequest que recibimos del formulario.
+	// Los items obtenidos serán cada uno de los campos del formulario,
+	// tanto campos normales como ficheros subidos.
+	List<?> items = upload.parseRequest(request);
+
+	File fichero = null;
+	// Se recorren todos los items, que son de tipo FileItem
+	for (Object item : items) {
+	   FileItem uploaded = (FileItem) item;
+
+	   // Hay que comprobar si es un campo de formulario. Si no lo es, se guarda el fichero
+	   // subido donde nos interese
+	   if (!uploaded.isFormField()) {
+	      // No es campo de formulario, guardamos el fichero en algún sitio
+	       fichero = new File(destino, uploaded.getName());
+	      uploaded.write(fichero);
+	   } else {
+	      // es un campo de formulario, podemos obtener clave y valor
+	      String key = uploaded.getFieldName();
+	      String valor = uploaded.getString();
+	   }
+	}
+	System.out.println(fichero.getAbsolutePath());
+	String direccionFinal=transformarDireccion(fichero.getAbsolutePath());
+	return ( direccionFinal);	
+}
+
+
+
+public String obtenerNombreImagen(String imagen)
+{
+	String path = imagen ;
+    String file = path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf("."));
+    String nombreImagen= file;
+    
+    return nombreImagen;
+}
+
+
+
+public String transformarDireccion(String Dir)
+{
+	String direccion = Dir;
+	char ruta[]= direccion.toCharArray();
+	char ruta2[]=new char[ruta.length+1];
+	int k=0;
+
+	for(int i=0;i<ruta.length;i++){
+		if(ruta[i]==':')
+		{
+			ruta2[k]=ruta[i];
+			k++;
+			ruta2[k]='/';
+		}
+		else if(ruta[i]=='\\')
+		{
+			ruta2[k]='/';
+		} 
+		else {
+			ruta2[k]=ruta[i];
+		}
+		k++;
+}//fin del for
+
+	direccion="";
+	for(int i=0;i<ruta2.length;i++)
+	{
+		direccion+=ruta2[i];
+		}
+	return direccion; //regresa el valor del array convertido en string
+	}
 }
--- src/main/java/Servlet/ServletSubirNegativos.java
+++ src/main/java/Servlet/ServletSubirNegativos.java
@@ -1,11 +1,19 @@
 package Servlet;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import Integracion.Visual;
 import logicaDeNegocios.Archivo;
@@ -32,10 +40,10 @@ public class ServletSubirNegativos extends HttpServlet {
 	{
 
 		
-		Archivo archivo= new Archivo();
+		
 		String rutaN = null;
 		try {
-			rutaN=  archivo.CargarArchivo(request, response);
+			rutaN=  CargarArchivo(request, response);
 			//archivo.EscribirArchivoTXT(ruta);
 		} 
 		catch (Exception e) 
@@ -54,5 +62,79 @@ public class ServletSubirNegativos extends HttpServlet {
 		request.getRequestDispatcher("EntrenarProfesor.jsp").forward(request, response);
 	  	
 	}
+	public String CargarArchivo(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		//Ruta donde se guardara el fichero
+		//File destino=new File("C:\\Users\\Cora\\Desktop\\profesorDigitalCognitivoTheWatsonAvenger");
+		File destino=new File("D:\\Users\\Esteban Morales\\git\\profesorDigitalCognitivoTheWatsonAvenger");
+		FileItemFactory factory = new DiskFileItemFactory();
+		ServletFileUpload upload = new ServletFileUpload(factory);
+
+
+		// req es la HttpServletRequest que recibimos del formulario.
+		// Los items obtenidos serán cada uno de los campos del formulario,
+		// tanto campos normales como ficheros subidos.
+		List<?> items = upload.parseRequest(request);
+
+		File fichero = null;
+		// Se recorren todos los items, que son de tipo FileItem
+		for (Object item : items) {
+		   FileItem uploaded = (FileItem) item;
+
+		   // Hay que comprobar si es un campo de formulario. Si no lo es, se guarda el fichero
+		   // subido donde nos interese
+		   if (!uploaded.isFormField()) {
+		      // No es campo de formulario, guardamos el fichero en algún sitio
+		       fichero = new File(destino, uploaded.getName());
+		      uploaded.write(fichero);
+		   } else {
+		      // es un campo de formulario, podemos obtener clave y valor
+		      String key = uploaded.getFieldName();
+		      String valor = uploaded.getString();
+		   }
+		}
+		System.out.println(fichero.getAbsolutePath());
+		String direccionFinal=transformarDireccion(fichero.getAbsolutePath());
+		return ( direccionFinal);
+		
+	}
+	
+	
+
+	
+
+	
+	
+	public String transformarDireccion(String Dir)
+	{
+		String direccion = Dir;
+		char ruta[]= direccion.toCharArray();
+		char ruta2[]=new char[ruta.length+1];
+		int k=0;
+
+		for(int i=0;i<ruta.length;i++){
+			if(ruta[i]==':')
+			{
+				ruta2[k]=ruta[i];
+				k++;
+				ruta2[k]='/';
+			}
+			else if(ruta[i]=='\\')
+			{
+				ruta2[k]='/';
+			} 
+			else {
+				ruta2[k]=ruta[i];
+			}
+			k++;
+	}//fin del for
+
+		direccion="";
+		for(int i=0;i<ruta2.length;i++)
+		{
+			direccion+=ruta2[i];
+			}
+		return direccion; //regresa el valor del array convertido en string
+		}
 
 }
